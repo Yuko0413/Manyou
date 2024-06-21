89c8c66ece00509a1193e7aa604af97bb31a24d4
Yuko0413 <soc13soc0422@gmail.com>
2024-06-21 16:16:39 +0900

    3回

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,6 +4,14 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '登録機能' do
     context 'タスクを登録した場合' do
       it '登録したタスクが表示される' do
+        visit new_task_path
+
+        fill_in 'Title', with: '書類作成'
+        fill_in 'Content', with: '企画書を作成する'
+        click_button 'Create Task'
+
+        expect(page). to have_content'書類作成'
+        expect(page). to have_content'企画書を作成する'
       end
     end
   end
@@ -11,7 +19,8 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '登録済みのタスク一覧が表示される' do
-        Task.create!(title: '書類作成', content: '企画書を作成する。')
+        #Task.create!(title: '書類作成', content: '企画書を作成する。')
+        FactoryBot.create(:task)
         visit tasks_path
         expect(page).to have_content '書類作成'
       end
@@ -21,6 +30,11 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it 'そのタスクの内容が表示される' do
+        #task = Task.create!(title: '書類作成', content: '企画書を作成する')
+        task = FactoryBot.create(:task)
+        visit task_path(task)
+        expect(page).to have_content '書類作成'
+        expect(page).to have_content '企画書を作成する'
        end
      end
   end
