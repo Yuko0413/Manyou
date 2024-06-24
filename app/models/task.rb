e67a99ef73fcf05416ab6216db3ef41ac0b35947
Yuko0413 <soc13soc0422@gmail.com>
2024-06-24 23:55:07 +0900

    ブランチ

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+
+  enum priority: { low: 0, medium: 1, high: 2 }
+  enum status: { not_started: 0, in_progress: 1, completed: 2 }
 end
