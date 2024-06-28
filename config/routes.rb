0526ce28dfed873ce8bcb8745ce196d662590414
Yuko0413 <soc13soc0422@gmail.com>
2024-06-28 17:55:38 +0900

    third

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   root 'tasks#index'
 
   # セッションのルーティング
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
 
+  resources :sessions
   resources :tasks
   resources :users, only: [:new, :create, :edit, :update, :show]
 
