75d0644254bd66c4099df4618f6e45eccc58b754
Yuko0413 <soc13soc0422@gmail.com>
2024-07-03 03:14:00 +0900

    20240703 16:12

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,16 @@ class User < ApplicationRecord
   has_secure_password
   has_many :tasks, dependent: :destroy
 
-
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
-  #validates :password_confirmation, presence: true, on: :create
   validates :admin, inclusion: { in: [true, false] }
 
   before_destroy :ensure_an_admin_remains
   before_update :ensure_an_admin_remains_on_update
 
   private
+
 
   def ensure_an_admin_remains
     if User.where(admin: true).count == 1 && self.admin?
@@ -22,9 +21,17 @@ class User < ApplicationRecord
   end
 
   def ensure_an_admin_remains_on_update
-    if User.where(admin: true).count == 1 && self.admin? && self.admin_changed?(from: true, to: false)
+    if User.where(admin: true).count == 1 && self.admin? && !self.admin
       errors.add(:base, '管理者が0人になるため権限を変更できません')
       throw :abort
+    end
+
+    # `admin` フラグが変更された場合の条件を追加
+    if self.admin_changed? && self.admin_was && !self.admin
+      if User.where(admin: true).count == 1
+        errors.add(:base, '管理者が0人になるため権限を変更できません')
+        throw :abort
+      end
     end
   end
 end
